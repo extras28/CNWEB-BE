729551f4729f828f6586baa93a08c91efb64a7ce
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2022-11-07 09:21:09 +0700

    request to reset passsword feature

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,7 +1,11 @@
 const Account = require("../models/account");
-const { generateRandomStr, sha256 } = require("../utils");
+const {
+    generateRandomStr,
+    sha256
+} = require("../utils");
 const utils = require("../utils");
 const moment = require("moment");
+const sendEmail = require("../utils/nodeMailer");
 
 const accountController = {
     signUp: async (req, res) => {
@@ -126,7 +130,55 @@ const accountController = {
             });
         }
     },
-    requestToResetPassword: async (req, res) => {},
+    requestToResetPassword: async (req, res) => {
+        try {
+            let {
+                email
+            } = req.body;
+
+            let account = await Account.findOne({
+                email: email
+            });
+
+            if (!account) {
+                return res.send({
+                    result: 'failed',
+                    message: 'email không hợp lệ'
+                })
+            };
+            var random = 100000 + Math.random() * 900000;
+            var plainResetPasswordToken = Math.floor(random);
+
+            var expirationDate = new Date();
+            var time = expirationDate.getTime();
+            var time1 = time + 5 * 60 * 1000;
+            var setTime = expirationDate.setTime(time1);
+            var expirationDateStr = moment(setTime)
+                .format("YYYY-MM-DD HH:mm:ss")
+                .toString();
+
+            await Account.findOneAndUpdate({
+                email: email
+            }, {
+                resetPasswordToken: plainResetPasswordToken,
+                expirationDateResetPasswordToken: expirationDateStr
+            });
+
+
+
+            res.send({
+                result: 'success',
+                expirationDate: moment(expirationDate).toDate(),
+            });
+
+            await sendEmail(email, 'CodeHelper your reset password code', plainResetPasswordToken);
+        } catch (error) {
+            res.send({
+                result: 'failed',
+                message: error
+            })
+        }
+    },
     resetPassword: async (req, res) => {},
     changePassword: async (req, res) => {
         try {
