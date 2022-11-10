b25ad9001e4eac64bed4267e8b240b27215444ba
nguyenanhdung281101 <nguyenanhdung2811@gmail.com>
2022-11-10 09:24:00 +0700

    update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(accountRouter, function(req, res, next){
     next();
 })
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Your app listening at http://localhost:${PORT}`);
 });
